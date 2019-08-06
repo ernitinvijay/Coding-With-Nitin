48f31f008ea48b5d43761c6809f51d2238a0e15a
ernitinvijay <45433440+ernitinvijay@users.noreply.github.com>
2019-08-07 00:54:51 +0530

    Update Employee.cs

--- Demo.Entity/Employee.cs
+++ Demo.Entity/Employee.cs
@@ -7,12 +7,9 @@ using System.Data.Entity;
 namespace Demo.Entity
 {
     public class Employee 
-    {
-      
+    {      
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public string Address { get; set; }
     }
 }
